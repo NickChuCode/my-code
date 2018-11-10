cc5842264f846ea2f8939fda447f5f103c296256
NickChuCode <nickchu15@outlook.com>
2018-11-10 09:34:23 +0800

    add 86 in ScriptOJ

---
+++ frontend/scriptOJ/medium/86.字体高亮函数.js
@@ -0,0 +1,20 @@
+// 请你完成 highlight 函数，可以把模版字符串中的插入内容替换掉，并且插入文档以后显示红色。例如：
+//
+// const yourName = 'ScriptOJ'
+// const myName = 'Jerry'
+// document.body.innerHTML = highlight`Hello, ${yourName}. I am ${myName}.`
+
+// /* 你可能需要样式 */
+// span {
+//   color: red;
+// }
+
+// 主要考察了template string的用法，他的用法是很灵活的，作为参数时，可以拆开，
+// 而且可以通过拆开后的数组检索到其中的string部分和表达式部分
+const highlight = (strings, ...values) => {
+    var res = ""
+    for (var i = 0; i < values.length; i++) {
+        res += strings[i] + `<span>${values[i]}</span>`
+    }
+    return res
+}
